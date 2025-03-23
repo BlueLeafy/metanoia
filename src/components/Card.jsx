905568ce2f45fae88f6ad3f135fd459b2c9aaf28
giGina818 <xxx@maindesktop.home>
2025-03-23 15:11:20 +0100

    initial commit

--- src/components/Card.jsx
+++
@@ -1,9 +0,0 @@
-function Card({ children, className = '' }) {
-    return (
-        <div className={`rounded-2xl shadow-2xl shadow-neutral-400 ${className}`}>
-            {children}
-        </div>
-    );
-};
-
-export default Card;
